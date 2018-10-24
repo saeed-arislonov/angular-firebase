01f7749d0fe3b666c0473f00b1712c1bdff77511
Saidbek Arislonov <saeed@Saidbeks-MacBook-Pro.local>
2018-10-24 21:05:03 +0500

    last changes

--- src/app/components/client-details/client-details.component.ts
+++ src/app/components/client-details/client-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Client } from '../../models/Client';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-client-details',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./client-details.component.scss']
 })
 export class ClientDetailsComponent implements OnInit {
+  id: string;
+  client: Client;
+  hasBalance = false;
+  showBalanceUpdateInput = false;
 
-  constructor() { }
+
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+
+    this.id = this.route.snapshot.params['id'];
+
+    this.clientService.getClient(this.id).subscribe(client => {
+      if (client != null) {
+        if (client.balance > 0) {
+          this.hasBalance = true;
+        }
+      }
+      if (client.balance === null) {
+        client.balance = 0;
+      }
+      this.client = client;
+    });
+  }
+
+  updateBalance() {
+    this.clientService.updateBalance(this.client);
+    this.flashMessage.show('Balance updated', {
+      cssClass: 'alert alert-success',
+      timeout: 3000
+    });
   }
 
 }
